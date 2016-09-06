93a8b33f3e09a80e596a4bed07d8c2e253600fb1
Artem Karpovich <karpartem94@gmail.com>
2016-09-06 14:10:58 +0300

    add routes which return details category, location categories

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,28 @@
 import express from 'express';
+import _ from 'lodash';
+import locationsInfo from '../locationsInfo.json';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.json('Hello world');
+router.get('/location-categories', (req, res) => {
+  const locationCategories = locationsInfo.map(categoryInfo => ({
+    id: categoryInfo.id,
+    category: categoryInfo.category
+  }));
+
+  res.json(locationCategories);
+});
+
+router.post('/details', (req, res) => {
+  const categoryId = req.body.categoryId;
+
+  const categoryDetails = _(locationsInfo)
+    .filter(category => category.id === categoryId)
+    .map(category => category.detailsCategoryItems)
+    .flatten()
+    .value();
+
+  res.json(categoryDetails);
 });
 
 export default router;
