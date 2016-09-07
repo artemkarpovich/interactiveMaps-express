287dd6f98565deb7dda5c18931692360f6d7d533
Artem Karpovich <karpartem94@gmail.com>
2016-09-07 13:26:20 +0300

    change structure essences

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,28 +1,21 @@
 import express from 'express';
 import _ from 'lodash';
-import locationsInfo from '../locationsInfo.json';
+import locations from '../locations.json';
+import locationCategories from '../locationCategories.json';
 
 const router = express.Router();
 
 router.get('/location-categories', (req, res) => {
-  const locationCategories = locationsInfo.map(categoryInfo => ({
-    id: categoryInfo.id,
-    category: categoryInfo.category
-  }));
 
   res.json(locationCategories);
 });
 
 router.post('/details', (req, res) => {
-  const categoryId = req.body.categoryId;
+  const categoryName = req.body.categoryName;
 
-  const categoryDetails = _(locationsInfo)
-    .filter(category => category.id === categoryId)
-    .map(category => category.detailsCategoryItems)
-    .flatten()
-    .value();
+  const locationsByCategory = _.filter(locations, item => item.category === categoryName);
 
-  res.json(categoryDetails);
+  res.json(locationsByCategory);
 });
 
 export default router;
